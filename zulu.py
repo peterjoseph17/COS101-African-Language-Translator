4754dc8e11042dc3c201f198d998709778112107
Emessiri oghenetega <tegaemmanuel1456@gmail.com>
2025-12-19 23:57:40 +0300

    Create zulu.py

---
+++ zulu.py
@@ -0,0 +1,23 @@
+# zulu.py
+zulu_dictionary = {
+    "hello": "sawubona",
+    "goodbye": "hamba kahle",
+    "thank you": "ngiyabonga",
+    "yes": "yebo",
+    "no": "cha",
+    "water": "amanzi",
+    "food": "ukudla",
+    "house": "indlu",
+    "person": "umuntu",
+    "child": "ingane",
+    "man": "indoda",
+    "woman": "owesifazane",
+    "love": "uthando",
+    "friend": "umngane",
+    "school": "isikole",
+    "teacher": "uthisha",
+    "book": "incwadi",
+    "money": "imali",
+    "work": "umsebenzi",
+    "market": "imakethe"
+}
